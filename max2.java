116ce44ca37151eba215f95a815752737cbef163
Abhilash-2215 <abhilash81354@gmail.com>
2026-01-31 12:13:50 +0530

    Add files via upload

---
+++ max2.java
@@ -0,0 +1,19 @@
+
+class max2
+{   
+    public static void main(String[] args)
+    {
+        int a= 5;
+        int =10;
+        if(a>b)
+        {
+            System.out.println("a is max"+a);
+        }
+        else
+        {
+            System.out.println(" b is max");
+
+        }
+    }
+
+}
