3b1ffb49a6c8b5aef0e7f438fa1c87d944c812cc
Abhilash-2215 <abhilash81354@gmail.com>
2026-01-31 15:00:04 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Test2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value of a:");
+        int a = sc.nextInt();
+        System.out.println("enter value of b:");
+
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("value of a  is:"+a);
+        System.out.println("value of b  is:"+b);
+        System.out.println("sum is "+c);
+        
+    }
+}
