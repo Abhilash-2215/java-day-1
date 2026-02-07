47f136e07373725736dfd65d888131255d852406
Abhilash-2215 <abhilash81354@gmail.com>
2026-02-07 10:06:39 +0530

    Add files via upload

---
+++ IntData.java
@@ -0,0 +1,15 @@
+public class IntData
+{
+    public static void main(String args[])
+    {
+        int a=100;
+        int b=071;
+        int c=0x90;
+        int d=0b1001;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
