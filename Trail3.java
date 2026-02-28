6fa697f0f9c48479aaf29c26713b18c661df6cf7
Abhilash-2215 <abhilash81354@gmail.com>
2026-02-28 11:39:43 +0530

    Add files via upload

---
+++ Trail3.java
@@ -0,0 +1,21 @@
+class Trail3
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        int n=5;
+        
+
+    
+    for (i=1;i<=n;i++){
+        for (j=i;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+    
+        System.out.println("");
+    }
+    }
+
+
+}
---
+++ trai2.java
@@ -0,0 +1,20 @@
+class Trai2
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        int n=5;
+        
+
+    
+    for (i=0;i<=n-1;i++){
+        for (j=0;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+    
+        System.out.println("");
+    }
+    }
+
+}
